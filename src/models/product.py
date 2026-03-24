42d65c8f8d1c54402803f14aebd626828534fa37
MirkoKiwi <mirko.cenedese@outlook.com>
2026-03-24 19:35:14 +0100

    Added Create, Update, Read forms for Product

--- src/models/product.py
+++ src/models/product.py
@@ -2,19 +2,40 @@ from sqlmodel import SQLModel, Field
 from sqlalchemy import Column, Numeric
 
 from decimal import Decimal
-from datetime import datetime
 
 
 
 
 class Product(SQLModel, table=True):
+    """Database table model"""
     id:       int | None = Field(default=None, primary_key=True)
-    name:     str
-    price:    Decimal = Field(sa_column=Column(Numeric(10, 2)))     # 10 Digits before comma, 2 after comma
+    name:     str        = Field(index=True)
+    price:    Decimal    = Field(sa_column=Column(Numeric(10, 2)))
     quantity: int
 
 
-class ProductForm(SQLModel):
+
+
+class ProductCreate(SQLModel):
+    """Creation of a new product"""
+    
+    name:     str     = Field(min_length=1, max_length=255)
+    price:    Decimal = Field(gt=0, decimal_places=2)
+    quantity: int     = Field(ge=0)
+
+
+class ProductUpdate(SQLModel):
+    """Update for an existing product"""
+    
+    name:     str     | None = Field(default=None, min_length=1, max_length=255)
+    price:    Decimal | None = Field(default=None, gt=0, decimal_places=2)
+    quantity: int     | None = Field(default=None, ge=0)
+
+
+class ProductRead(SQLModel):
+    """Client read only"""
+    
+    id:       int
     name:     str
     price:    Decimal
     quantity: int
