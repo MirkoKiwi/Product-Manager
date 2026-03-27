e65d930e0e9f8235169157008db148c377d9b825
MirkoKiwi <mirko.cenedese@outlook.com>
2026-03-27 17:11:44 +0100

    Added database initialization setup and session dependency

--- src/data/db.py
+++ src/data/db.py
@@ -1,11 +1,17 @@
 from sqlmodel import create_engine, SQLModel, Session
-
 import os
+from typing import Annotated
+from fastapi import Depends
+
+from src.config import config
+
+
+# DB Models
+from src.models.product import Product, ProductCreate, ProductUpdate, ProductRead
 
 
 
-
-sqlite_file_name = config.root_dir / "data/products.db"
+sqlite_file_name = config.root_dir / "src/data/products.db"
 sqlite_url       = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
@@ -13,6 +19,15 @@ engine       = create_engine(sqlite_url, connect_args=connect_args, echo=True)  
 
 
 
+
 def init_database() -> None:
     does_exist: bool = os.path.isfile(sqlite_file_name)
-    pass
+    SQLModel.metadata.create_all(engine) 
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
